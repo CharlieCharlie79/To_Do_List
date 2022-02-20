855c1c9150ceeccd9c1bfd1eafbf9a3c7daed877
CharlieCharlie79 <carloswork067@hotmail.com>
2022-02-19 23:16:04 -0500

    Check if icon has been clicked-done

---
+++ JS/home.js
@@ -0,0 +1,15 @@
+var icon = document.getElementById("plus-icon");
+
+
+//Check if the plus icon has been clicked
+icon.addEventListener("click", ()=>
+{
+    console.log("It has been clicked");
+});
+
+/*
+function spin_animation()
+{ 
+}
+spin_animation();
+*/
